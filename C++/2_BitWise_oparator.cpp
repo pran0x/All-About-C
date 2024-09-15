7acd275930e7c3a96b9caca4b0fbad66737f547d
Pranto Kumar <prantokumar.net@gmail.com>
2024-09-15 14:39:29 +0600

    BitWise operator

---
+++ C++/2_BitWise_oparator.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int a = 6, b = 10;
+cout<<"(&) AND Operator \t "<< (a & b) << endl;
+cout<<"(|) OR Operator  \t"<< (a | b) << endl;
+cout<<"(^) XOR Operator \t"<< (a ^ b) << endl;
+cout<<"(<<) LEFT-Shift Operator \t"<< (10 << 2) << endl;
+cout<<"(>>) RIGHT-Shift Operator \t"<< (10 >> 1) << endl;
+return 0;
+}
