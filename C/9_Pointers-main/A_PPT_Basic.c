3c7e4be3793995172476500e7bbea58277ab61da
Pranto Kumar <prantokumar.net@gmail.com>
2024-12-13 23:36:50 +0600

    added~address printed

--- C/9_Pointers-main/A_PPT_Basic.c
+++ C/9_Pointers-main/A_PPT_Basic.c
@@ -4,6 +4,9 @@ int main(int argc, char const *argv[])
     int i = 5 ;
     int *ptr = &i;
     int **pptr = &ptr;
-    printf("%d\n",**pptr);
+    printf("%d\n",&i);
+    printf("%d\n",&ptr);
+    printf("%d\n",&pptr);
+    pptr = ++ptr;
     return 0;
 }
